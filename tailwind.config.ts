479873496e3ef72688e772558c18bee2139820e5
maxim <bozzhik@ya.ru>
2024-10-03 13:11:45 +0300

    set up `shadcn/ui` into project
    npx shadcn@latest init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,16 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss'
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [tailwindcssAnimate],
+}
+export default config
