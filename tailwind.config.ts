447f645f82a64e320ba85b714aa9a363c1b29936
maxim <bozzhik@ya.ru>
2024-10-03 13:14:27 +0300

    set up `tailwind` configuration + set typescript `alliases`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,34 @@
 import type {Config} from 'tailwindcss'
+import plugin from 'tailwindcss/plugin'
+
 import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config: Config = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-      },
+    screens: {
+      xl: {max: '1536px'},
+      lg: {max: '1280px'},
+      // md: {max: '1024px'},
+      sm: {max: '428px'},
     },
+    colors: {
+      background: {
+        DEFAULT: 'var(--background)',
+        alt: 'var(--background-alt)',
+      },
+      foreground: 'var(--foreground)',
+      card: 'var(--card)',
+      red: 'var(--red)',
+    },
+    extend: {},
   },
-  plugins: [tailwindcssAnimate],
+  plugins: [
+    plugin(function sizePlugin(api) {
+      api.matchUtilities({s: (value) => ({width: value, height: value})}, {values: api.theme('width')})
+    }),
+    tailwindcssAnimate,
+  ],
 }
+
 export default config
