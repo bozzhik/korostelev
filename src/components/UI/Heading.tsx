92c9d316efa7a89e2b3440a2467c5810bb097230
maxim <bozzhik@ya.ru>
2024-10-20 15:36:18 +0300

    init `Heading` UI component

---
+++ src/components/UI/Heading.tsx
@@ -0,0 +1,18 @@
+import {cn} from '@/lib/utils'
+
+interface Props {
+  type: 'h1' | 'h2'
+  text: string
+  className?: string
+}
+
+export const headingClasses = {
+  h1: 'text-[145px] leading-[1.1] font-extralight tracking-tight',
+  h2: 'text-6xl font-medium uppercase !leading-[1.25]',
+}
+
+export default function Heading({type, text, className}: Props) {
+  const Heading = type
+
+  return <Heading className={cn(headingClasses[type], '', className)} dangerouslySetInnerHTML={{__html: text || ''}} />
+}
