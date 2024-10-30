ea6f2e49887ca0980acac48b28505be17c565712
maxim <bozzhik@ya.ru>
2024-10-31 00:11:32 +0300

    ~ implementing `client` changes

--- src/components/App/index/Contacts.tsx
+++ src/components/App/index/Contacts.tsx
@@ -69,7 +69,7 @@ export default function Contacts() {
         <div className="flex flex-col h-full gap-7 sm:gap-5">
           <Input content="text" placeholder="Ваше имя" />
           <Input content="tel" placeholder="Номер телефона" />
-          <Input content="email" placeholder="E-mail" />
+          <Input content="email" placeholder="Электронная почта" />
           <div className="flex flex-col justify-between h-full">
             <Input type="textarea" className="h-full" placeholder="Чем я могу помочь?" rows={!isMobile ? 2 : 4} />
             <Button variant="solid" mode="dark" className="w-full" text="Отправить форму" />
