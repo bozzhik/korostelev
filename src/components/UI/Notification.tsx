fc7a518b35961dfcdc78e26bc5780529dd93116b
maxim <bozzhik@ya.ru>
2024-11-03 00:28:26 +0300

    [2] adapt `Notification` UI component

--- src/components/UI/Notification.tsx
+++ src/components/UI/Notification.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import {isMobile} from '@bozzhik/is-mobile'
 import {useState, useEffect, useRef} from 'react'
 import Cookies from 'js-cookie'
 import {X} from 'lucide-react'
@@ -33,7 +34,9 @@ export default function Notification() {
     <section id="notification" className="pb-5 sm:p-3 w-full fixed bottom-0 left-0 grid place-items-center z-[99]">
       <div ref={notificationRef} className="w-[45%] xl:w-[60%] sm:w-full px-3 sm:p-2.5 flex sm:flex-col sm:gap-2.5 justify-between items-center bg-background-alt [box-shadow:0_11px_25px_-5px_#00000060]">
         <p className="text-[15px] sm:text-sm !leading-tight font-medium py-3 sm:py-0">Мы используем файлы cookie для организации работы сайта и повышения качества нашей работы. Продолжая использование сайта, вы соглашаетесь с приемом и передачей файлов cookie.</p>
-        <X className="duration-200 cursor-pointer s-12 sm:w-full sm:h-6 sm:py-1 sm:self-end hover:text-red sm:bg-foreground sm:text-background" size={20} onClick={() => setShow(false)} />
+        <div className="grid place-items-center sm:self-end sm:w-full sm:py-1 cursor-pointer group sm:bg-foreground sm:text-background" onClick={() => setShow(false)}>
+          {!isMobile ? <X className="s-8 xl:s-6 duration-200 group-hover:text-red" /> : 'Принять'}
+        </div>
       </div>
     </section>
   )
