46d49488bcbf45935276e99985916a539d4f9b2b
maxim <bozzhik@ya.ru>
2024-12-17 00:30:53 +0300

    upgrade `ServicesModule` using `framer-motion`
    https://ui.aceternity.com/components/card-hover-effect

--- src/components/App/index/ServicesModule.tsx
+++ src/components/App/index/ServicesModule.tsx
@@ -1,28 +1,69 @@
+'use client'
+
 import {TService} from '@/utils/getData'
+import {cn} from '@/lib/utils'
+
+import {useState} from 'react'
+import {AnimatePresence, motion} from 'framer-motion'
 
 import Text from '~/UI/Text'
 import {Modal as ModalTrigger} from '~/UI/DrawerModal'
 import {SplitText} from '~/UI/SplitText'
 
 export default function ServicesModule({data}: {data: [string, TService][]}) {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null)
+
   return (
-    <section data-section="desktop-services" className="flex flex-col sm:hidden">
-      {data.map(([key, {heading, description, content}]) => (
-        <ModalTrigger key={key} tag={'Услуга'} heading={heading} content={content}>
-          <hr className="h-[0.1rem] bg-foreground" />
+    <section data-section="desktop-services" className={cn('flex flex-col sm:hidden', 'border-t-2 divide-y-2 divide-foreground border-foreground')}>
+      {data.map(([key, {heading, description, content}], index) => (
+        <div
+          key={key} // unique key
+          className="relative"
+          onMouseEnter={() => setHoveredIndex(index)}
+          onMouseLeave={() => setHoveredIndex(null)}
+        >
+          <AnimatePresence>
+            {hoveredIndex === index && (
+              <motion.span
+                className="absolute inset-0 block w-full h-full bg-red"
+                layoutId="hoverBackground"
+                initial={{opacity: 0}}
+                animate={{
+                  opacity: 1,
+                  transition: {duration: 0.15},
+                }}
+                exit={{
+                  opacity: 0,
+                  transition: {duration: 0.15, delay: 0.2},
+                }}
+              />
+            )}
+          </AnimatePresence>
 
-          <div className="relative grid items-center grid-cols-2 gap-20 px-10 py-16 text-left duration-200 group hover:bg-red hover:text-background-alt">
-            <SplitText duration={1} y={0} className="absolute inset-9">
-              <div className="duration-200 rounded-full s-7 bg-foreground group-hover:bg-background-alt"></div>
-            </SplitText>
+          <ModalTrigger tag={'Услуга'} heading={heading} content={content}>
+            <div
+              className={cn('duration-200 relative grid items-center w-screen grid-cols-2 gap-20 px-10 py-16 text-left group', hoveredIndex === index ? 'text-background-alt' : 'text-foreground')}
+              style={{
+                transition: hoveredIndex === index ? 'color 0.15s ease' : 'color 0.15s ease 0.2s',
+              }}
+            >
+              <SplitText duration={1} y={0} className="absolute inset-9">
+                <div
+                  className={cn('duration-200 rounded-full s-7', hoveredIndex === index ? 'bg-background-alt' : 'bg-foreground')}
+                  style={{
+                    transition: hoveredIndex === index ? 'background-color 0.15s ease' : 'background-color 0.15s ease 0.2s',
+                  }}
+                ></div>
+              </SplitText>
 
-            <SplitText duration={0.5}>
-              <Text type="h4" className="ml-28 max-w-[20ch]" text={heading} />
-            </SplitText>
+              <SplitText duration={0.5}>
+                <Text type="h4" className="ml-28 max-w-[20ch]" text={heading} />
+              </SplitText>
 
-            <Text type="h5" className="max-w-[50ch]" text={description} />
-          </div>
-        </ModalTrigger>
+              <Text type="h5" className="max-w-[50ch]" text={description} />
+            </div>
+          </ModalTrigger>
+        </div>
       ))}
     </section>
   )
